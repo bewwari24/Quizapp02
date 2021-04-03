996cb25b8933d40acb8cde3bf2c10185768facd5
bewwari24 <77846729+bewwari24@users.noreply.github.com>
2021-04-03 22:36:08 +0700

    Quizapp02

---
+++ QuizAppgames/app/src/main/java/com/example/quizappgames/Score.kt
@@ -0,0 +1,20 @@
+package com.example.quizappgames
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+@Suppress("DEPRECATION")
+class Score : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_score)
+
+
+
+    }
+}
